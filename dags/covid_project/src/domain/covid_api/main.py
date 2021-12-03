8ce50f4ac08267aac2a8feb8e66a2bed20d153d3
Felipe Gomes <felipegomes540@gmail.com>
2021-12-03 20:29:29 -0300

    feat: load data to gcs

--- dags/covid_project/src/domain/covid_api/main.py
+++ dags/covid_project/src/domain/covid_api/main.py
@@ -1,9 +1,13 @@
-from extract import CovidApiExtract
+from covid_project.src.domain.covid_api.extract import CovidApiExtract
+from covid_project.src.external.gcs.loader import GCSLoader
 
 def el_script(
     date: str,
-    api_key: str
-    
+    api_key: str,
+    project_id: str,
+    gcs_credential: str
 ) -> None:
     extract = CovidApiExtract(date=date, key=api_key)
     daily_covid_csv_data = extract.extract_workflow()
+    loader_to_gcs = GCSLoader(project_id=project_id, csv_data=daily_covid_csv_data, credentials=gcs_credential)
+    loader_to_gcs.load_data()
