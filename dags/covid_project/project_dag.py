f4ec5ce8c0b3b23177689f213c4629ac08d52862
Felipe Gomes <felipegomes540@gmail.com>
2021-12-03 21:57:55 -0300

    test project

---
+++ dags/covid_project/project_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from covid_project.src.domain.covid_api.main import el_script
+
+default_args = {
+    "owner": "Felipe Gomes",
+    "start_date": datetime(2021, 1, 1),
+    "depends_on_past": False,
+}
+
+with DAG(
+    dag_id="elt_covid_project",
+    default_args=default_args,
+    catchup=False,
+) as dag:
+    start = DummyOperator(task_id="start")
+    
+    end = DummyOperator(task_id="end")
+    
+    el_project = PythonOperator(
+        task_id="covid_el",
+        python_callable=el_script,
+        provide_context=True,
+        op_kwargs={
+            "date": "{{ ds }}",
+            "api_key": Variable.get("covid_api_key"),
+            "project_id": Variable.get("project_id"),
+            "gcs_credential": Variable.get("gcs_service_account"),            
+        }
+    )
